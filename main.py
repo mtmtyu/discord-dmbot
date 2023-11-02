b3bbaf7333e3d2bcf252458644e998c6523010c4
mtmtyu <81827724+mtmtyu@users.noreply.github.com>
2023-11-03 01:43:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.channel.id == 1076190674770210843:
-        # 絵文字を追加
-        emoji ="👍"
-        await message.add_reaction(emoji)
-        # メッセージを投稿したユーザーにロールを追加
-        user = message.author
-        role_id = 1169189492511481896
-        role = discord.utils.get(user.guild.roles, id=role_id)
-        if role:
-            await user.add_roles(role)
-        # メッセージを投稿したユーザーにDMを送信
-        await message.author.send('もちもちサーバーにご参加ありがとうございます！自己紹介をしてくれたようですね！私からプレゼントがあります！サーバーに入った際に使ってみてください！それではサーバーでの生活をお楽しみください！　コマンド：/redeem WELCOME!MOCHISERVER')
+      # 絵文字を追加
+      emoji ="👍"
+      await message.add_reaction(emoji)
+      # メッセージを投稿したユーザーにロールを追加
+      user = message.author
+      add_role_id = 1169189492511481896
+      add_role = discord.utils.get(user.guild.roles, id=add_role_id)
+      if add_role:
+            await user.add_roles(add_role)
+      remove_role_id = 1169630629210632252
+      remove_role = discord.utils.get(user.guild.roles, id=remove_role_id)
+      if remove_role:
+        await user.remove_roles(remove_role)
+        
+      # メッセージを投稿したユーザーにDMを送信
+      await message.author.send('もちもちサーバーにご参加ありがとうございます！自己紹介をしてくれたようですね！私からプレゼントがあります！サーバーに入った際に使ってみてください！それではサーバーでの生活をお楽しみください！　コマンド：/redeem WELCOME!MOCHISERVER')
 
 from keep_alive import keep_alive
 keep_alive()
